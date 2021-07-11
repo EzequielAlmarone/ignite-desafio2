7d9ac5cd0e1d29ccef3247d4752e58df87139048
Ezequiel Almarone <ezequiel.almarone@gmail.com>
2021-07-11 15:47:01 -0400

    :tada: desafio finalizado

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,37 @@
-export function SideBar() {
+import { Button } from '../components/Button';
+import '../styles/sidebar.scss';
+
+interface SidebarProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface GenresProps {
+  genres: SidebarProps[];
+  selectedGenreId: number;
+  handleClickButton(id: number): void;
+}
+export function SideBar( {genres, selectedGenreId, handleClickButton}:GenresProps) {
   // Complete aqui
+  return (
+    <>
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map( (genre: SidebarProps) => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+    </>
+  )
 }
